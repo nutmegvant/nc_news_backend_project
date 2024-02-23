a72ab2a16689b1120a8e401290e52313c4a6a8ea
nutmegvant <nicola.grzebyk@gmail.com>
2024-02-23 16:15:02 +0000

    husky issue

--- db/connection.js
+++ db/connection.js
@@ -3,13 +3,18 @@ const ENV = process.env.NODE_ENV || 'development';
 
 const config = {};
 
-if (ENV === 'production') {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
 module.exports = new Pool(config);
+
